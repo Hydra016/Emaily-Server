f1bf6d6842457cc507a740b2c3562a8507cf8e52
Haider Mansoor <haider@bitfactory.io>
2023-04-28 23:42:25 +0300

    v1.0

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -10,11 +10,19 @@ module.exports = app => {
     }))
     app.get('/auth/facebook/callback', passport.authenticate('facebook'));
     app.get('/auth/google/callback', passport.authenticate('google'),  (req, res) => {
-        res.redirect(process.env.DEV_DASHBOARD_PATH);
+        if(process.env.NODE_ENV === 'production') { 
+            res.redirect(process.env.PROD_DASHBOARD_PATH);
+        } else {
+            res.redirect(process.env.DEV_DASHBOARD_PATH);
+        }
     });
     app.get('/api/logout', (req, res) => {
         req.logout();
-        res.redirect(process.env.DEV_ROOT_PATH)
+        if(process.env.NODE_ENV === 'production') {
+            res.redirect(process.env.PROD_ROOT_PATH)
+        } else {
+            res.redirect(process.env.DEV_ROOT_PATH)
+        }
     })
     app.get('/api/current_user', (req, res) => {
     if (req.isAuthenticated()) {
